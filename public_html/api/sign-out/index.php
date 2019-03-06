e6b491f46022fe7d82ef7c5d0b1b04695a4e25f4
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-06 10:49:00 -0700

    added code to sign-out api https://app.asana.com/0/1108530030234345/1112036483298270

--- public_html/api/sign-out/index.php
+++ public_html/api/sign-out/index.php
@@ -1,6 +1,51 @@
-<IfModule mod_rewrite.c>
-	RewriteEngine on
-	RewriteCond %{REQUEST_FILENAME} !-f
-	RewriteCond %{REQUEST_FILENAME} !-d
-	RewriteRule ^/?([\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12})?$ ?id=$1&%{QUERY_STRING}
-</IfModule>
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+
+use DeepDiveDatingApp\DeepDiveDating\User;
+
+/*
+ * api for signing out of deep dive dating account
+ *
+ * @author Kathleen Mattos <kmattos1@cnm.edu>
+ */
+
+//verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+
+//prepare the default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//grab the mySQL connection
+	$secrets = new \Secrets("");  // todo tell me secrets
+	$pdo = $secrets->getPdoObject();
+	//determine what method is used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["REQUEST_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET") {
+		$_SESSION =[];
+		$reply->message = "You are Now signed out.";
+	}
+	else {
+			throw (new \InvalidArgumentException("Invalid HTTP method request."));
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+//encode and return reply to front end caller
+echo json_encode($reply);
