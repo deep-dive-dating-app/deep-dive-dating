57a7d1adda61fee948116d8c9ad45167baf97580
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-09 12:35:55 -0700

    working on answer api https://app.asana.com/0/1108530030234345/1112036483298282

--- public_html/api/answer/index.php
+++ public_html/api/answer/index.php
@@ -13,3 +13,37 @@ use DeepDiveDatingApp\DeepDiveDating\{Answer, User};
  *
  * @author Kathleen Mattos
  */
+
+//verifying the session, or start if not active
+if (session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare and empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//get mysql connection
+	$secrets =  new \Secrets("/etc/apache2/capstone-mysql/cohort23/dateadan.ini");
+	$pdo = $secrets->getPdoObject();
+
+	// which http method used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	//sanitize the input
+	$answerUserId = filter_input(INPUT_GET, "answerUserId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$answerQuestionId = filter_input(INPUT_GET, "answerQusetionId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$answerResult = filter_input(INPUT_GET, "answerResult", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$answerScore = filter_input(INPUT_GET, "answerScore", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	//make sure the id is valid for methods that require it
+	if (($method === "DELETE" || $method === "PUT") && (empty($answerUserId) === true)){
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+
+
+
+
+}
