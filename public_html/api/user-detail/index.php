20174c5fd32c3a499d59a93bb8ef7e62f2c62015
Natalie Woodard <nwoodard1@cnm.edu>
2019-03-11 15:42:48 -0600

    Updated user detail with more appropriate methods and such

--- public_html/api/user-detail/index.php
+++ public_html/api/user-detail/index.php
@@ -1,56 +1,107 @@
 <?php
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/Classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+use DeepDiveDatingApp\DeepDiveDating\{User, UserDetail};
 
-// we determine if we have a GET request. If so, we then process the request.
-if ($method === "GET") {
+	/**
+	 * API for UserDetail
+	 *
+	 * @author Nwoodard1
+	 * @version 1.0
+	 **/
 
 
-// If it is not a GET request, we then proceed here to determine if we have a PUT or POST request.
-} else if($method === "PUT") {
+//verify the session, start if not active
+			if(session_status() !== PHP_SESSION_ACTIVE) {
+				session_start();
+			//}
 
-	//do setup that is needed for PUT request
+//prepare an empty reply
+			$reply = new stdClass();
+			$reply->status = 200;
+			$reply->data = null;
 
-	//perform the actual put
-	if($method === "PUT") {
-		// determines if we have a PUT request. If so we process the request.
-		// process PUT requests here
+			try {
+				//grab the mySQL connection
+				$secrets = new \Secrets("/etc/apache2/capstone-mysql/dateadan.ini");
+				$pdo = $secrets->getPdoObject();
 
-}
-else if($method === "PUT") {
+				//determine which HTTP method was used
+				$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
-	// enforce the user has a XSRF token
-	verifyXsrf();
+				//sanitize input
+				$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailId = filter_input(INPUT_GET, "userDetailId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailUserId = filter_input(INPUT_GET, "userDetailUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailAboutMe = filter_input(INPUT_GET, "userDetailAboutMe", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailAge = filter_input(INPUT_GET, "userDetailAge", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailCareer = filter_input(INPUT_GET, "userDetailCareer", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailDisplayEmail = filter_input(INPUT_GET, "userDetailDisplayEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailEducation = filter_input(INPUT_GET, "userDetailEducation", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailGender = filter_input(INPUT_GET, "userDetailGender", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailInterests = filter_input(INPUT_GET, "userDetailInterests", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailRace = filter_input(INPUT_GET, "userDetailRace", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				$userDetailReligion = filter_input(INPUT_GET, "userDetailReligion", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-	//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-	$requestContent = file_get_contents("php://input");
 
-	// This Line Then decodes the JSON package and stores that result in $requestObject
-	$requestObject = json_decode($requestContent);
 
-	//make sure user detail is available (required field)
-	if(empty($requestObject->userDetail) === true) {
-		throw(new \InvalidArgumentException ("No content for user detail", 405));
-	}
+				//make sure the id is valid for methods that require it
+				if(($method === "PUT") && (empty($id) === true)) {
+					throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+				} else if($method === "PUT" || $method === "POST") {
 
-	//perform the actual put
-	if($method === "PUT") {
+					// enforce the user has a XSRF token
+					verifyXsrf();
 
-		// retrieve the user detail to update
-		$userDetail = User::getUserDetailByUserId($pdo, $id);
-		if($userDetail === null) {
-			throw(new RuntimeException("user detail does not exist", 404));
-		}
+					//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+					$requestContent = file_get_contents("php://input");
 
-		//enforce the user is signed in and only trying to edit their own user detail
-		if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $userDetail->getUserId()->toString()) {
-			throw(new \InvalidArgumentException("You are not allowed to edit this user detail", 403));
-		}
+					// This Line Then decodes the JSON package and stores that result in $requestObject
+					$requestObject = json_decode($requestContent);
 
-		// update all attributes
-		$userDetail->setUserDetail($requestObject->userDetail);
-		$userDetail->update($pdo);
-	}
+					//make sure tweet content is available (required field)
+					if(empty($requestObject->userDetail) === true) {
+						throw(new \InvalidArgumentException ("No content for user detail.", 405));
+					}
 
-		// update reply
-		$reply->message = "User detail updated OK";
-	}
+
+					//perform the actual put or post
+					if($method === "PUT") {
+
+						// retrieve the tweet to update
+						$user = UserDetail::getUserDetailByUserDetailId($pdo, $id);
+						if($user === null) {
+							throw(new RuntimeException("user detail does not exist", 404));
+						}
+
+						//enforce the user is signed in and only trying to edit their own tweet
+						if(empty($_SESSION["userDetail"]) === true || $_SESSION["userDetail"]->getUserDetailId()->toString() !== $user->getUserDetailId()->toString()) {
+							throw(new \InvalidArgumentException("You are not allowed to edit this user detail", 403));
+						}
+
+						// update all attributes
+						$user->setUserDetail($requestObject->userDetail);
+						$user->update($pdo);
+
+						// update reply
+						$reply->message = "user detail updated OK";
+
+					}
+
+				}
+// update the $reply->status $reply->message
+			} catch(\Exception | \TypeError $exception) {
+				$reply->status = $exception->getCode();
+				$reply->message = $exception->getMessage();
+			}
+
+// encode and return reply to front end caller
+			header("Content-type: application/json");
+			echo json_encode($reply);
+
+// finally - JSON encodes the $reply object and sends it back to the front end.
 }
