ee997b0fa7ef3da5b35ae8ba4d80b373b926a8ad
Nehomah Mora <nehomahm@gmail.com>
2019-03-11 15:43:55 -0600

    created questions.sql https://app.asana.com/0/1108530030234345/1108530030234349

---
+++ sql/questions.sql
@@ -0,0 +1,73 @@
+ALTER	DATABASE dateadan CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS question;
+
+INSERT INTO question1 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question2 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question3 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question4 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question5 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question6 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question7 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question8 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question9 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
+
+INSERT INTO question10 (
+	questionId BINARY(16) NOT NULL,
+	questionContent VARCHAR (128) NOT NULL,
+	questionValue TINYINT (1) NOT NULL,
+	PRIMARY KEY(questionId)
+);
