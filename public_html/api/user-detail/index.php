73d02db74eed986fc7f93664a9ad4ec1a6c47bb2
Natalie Woodard <nwoodard1@cnm.edu>
2019-03-11 19:49:40 -0600

    cleaned up ALL OF THE messy code in user-detail api to make finding errors easier and better readability

--- public_html/api/user-detail/index.php
+++ public_html/api/user-detail/index.php
@@ -57,90 +57,90 @@ use DeepDiveDatingApp\DeepDiveDating\UserDetail;
 					verifyXsrf();
 
 					//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-					$requestContent = file_get_contents("php://input");
+				$requestContent = file_get_contents("php://input");
 
-					// This Line Then decodes the JSON package and stores that result in $requestObject
-					$requestObject = json_decode($requestContent);
+				// This Line Then decodes the JSON package and stores that result in $requestObject
+				$requestObject = json_decode($requestContent);
 
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailUserId) === true) {
-						throw(new \InvalidArgumentException ("No detail user id for user detail.", 405));
-					}
-
-					//make sure user detail user about me(required field)
-					if(empty($requestObject->userDetailAboutMe) === true) {
-						throw(new \InvalidArgumentException ("No about me for user detail.", 405));
-					}
-					//make sure user detail age (required field)
-					if(empty($requestObject->userDetailAge) === true) {
-						throw(new \InvalidArgumentException ("No age for user detail.", 405));
-					}
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailCareer) === true) {
-						throw(new \InvalidArgumentException ("No career for user detail.", 405));
-					}
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailDisplayEmail) === true) {
-						throw(new \InvalidArgumentException ("No display email for user detail.", 405));
-					}
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailEducation) === true) {
-						throw(new \InvalidArgumentException ("No education for user detail.", 405));
-					}
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailGender) === true) {
-						throw(new \InvalidArgumentException ("No gender for user detail.", 405));
-					}
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailInterests) === true) {
-						throw(new \InvalidArgumentException ("No interests for user detail.", 405));
-					}
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailRace) === true) {
-						throw(new \InvalidArgumentException ("No race for user detail.", 405));
-					}
-					//make sure user detail user id (required field)
-					if(empty($requestObject->userDetailReligion) === true) {
-						throw(new \InvalidArgumentException ("No religion for user detail.", 405));
-					}
-					//perform the actual put
-					if($method === "PUT") {
-
-					// retrieve the user detail to update
-					$userDetail = UserDetail::getUserDetailByUserDetailId($pdo, $id);
-					if($userDetail === null) {
-						throw(new RuntimeException("user detail does not exist", 404));
-						}
-
-					//enforce the user is signed in and only trying to edit their own user detail
-					if(empty($_SESSION["userDetail"]) === true || $_SESSION["userDetail"]->getUserDetailId()->toString() !== $userDetail->getUserDetailId()->toString()) {
-						throw(new \InvalidArgumentException("You are not allowed to edit this user detail", 403));
-						}
-
-					// update all attributes
-					$userDetail->setUserDetail($requestObject->userDetail);
-					$userDetail->update($pdo);
-
-						// update reply
-						$reply->message = "user detail updated OK";
-					}
-					// enforce the user is signed in
-					if(empty($_SESSION["userDetail"]) === true) {
-						throw(new \InvalidArgumentException("you must be logged in to update user detail", 403));
-					}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailUserId) === true) {
+					throw(new \InvalidArgumentException ("No detail user id for user detail.", 405));
 				}
-					$userDetail->setUserDetailUser($requestObject->userDetailUserId);
-					$userDetail->setUserDetailAboutMe($requestObject->userDetailAboutMe);
-					$userDetail->setUserDetailAge($requestObject->userDetailAge);
-					$userDetail->setUserDetailCareer($requestObject->userDetailCareer);
-					$userDetail->setUserDetailDisplayEmail($requestObject->userDetailDisplayEmail);
-					$userDetail->setUserDetailEducation($requestObject->userDetailEducation);
-					$userDetail->setUserDetailGender($requestObject->userDetailGender);
-					$userDetail->setUserDetailInterests($requestObject->userDetailInterests);
-					$userDetail->setUserDetailRace($requestObject->userDetailRace);
-					$userDetail->setUserDetailReligion($requestObject->userDetailReligion);
 
-					// update the $reply->status $reply->message
+				//make sure user detail user about me(required field)
+				if(empty($requestObject->userDetailAboutMe) === true) {
+					throw(new \InvalidArgumentException ("No about me for user detail.", 405));
+				}
+				//make sure user detail age (required field)
+				if(empty($requestObject->userDetailAge) === true) {
+					throw(new \InvalidArgumentException ("No age for user detail.", 405));
+				}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailCareer) === true) {
+					throw(new \InvalidArgumentException ("No career for user detail.", 405));
+				}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailDisplayEmail) === true) {
+					throw(new \InvalidArgumentException ("No display email for user detail.", 405));
+				}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailEducation) === true) {
+					throw(new \InvalidArgumentException ("No education for user detail.", 405));
+				}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailGender) === true) {
+					throw(new \InvalidArgumentException ("No gender for user detail.", 405));
+				}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailInterests) === true) {
+					throw(new \InvalidArgumentException ("No interests for user detail.", 405));
+				}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailRace) === true) {
+					throw(new \InvalidArgumentException ("No race for user detail.", 405));
+				}
+				//make sure user detail user id (required field)
+				if(empty($requestObject->userDetailReligion) === true) {
+					throw(new \InvalidArgumentException ("No religion for user detail.", 405));
+				}
+				//perform the actual put
+				if($method === "PUT") {
+
+				// retrieve the user detail to update
+				$userDetail = UserDetail::getUserDetailByUserDetailId($pdo, $id);
+				if($userDetail === null) {
+					throw(new RuntimeException("user detail does not exist", 404));
+				}
+
+				//enforce the user is signed in and only trying to edit their own user detail
+				if(empty($_SESSION["userDetailId"]) === true || $_SESSION["userDetail"]->getUserDetailId()->toString() !== $userDetail->getUserDetailId()->getBytes()) {
+					throw(new \InvalidArgumentException("You are not allowed to edit this user detail", 403));
+				}
+
+				// update all attributes
+				$userDetail->setUserDetailId($requestObject->userDetailId);
+				$userDetail->update($pdo);
+
+				// update reply
+				$reply->message = "user detail updated OK";
+				}
+				// enforce the user is signed in
+				if(empty($_SESSION["userDetail"]) === true) {
+					throw(new \InvalidArgumentException("you must be logged in to update user detail", 403));
+				}
+			}
+				$userDetail->setUserDetailUserId($requestObject->userDetailUserId);
+				$userDetail->setUserDetailAboutMe($requestObject->userDetailAboutMe);
+				$userDetail->setUserDetailAge($requestObject->userDetailAge);
+				$userDetail->setUserDetailCareer($requestObject->userDetailCareer);
+				$userDetail->setUserDetailDisplayEmail($requestObject->userDetailDisplayEmail);
+				$userDetail->setUserDetailEducation($requestObject->userDetailEducation);
+				$userDetail->setUserDetailGender($requestObject->userDetailGender);
+				$userDetail->setUserDetailInterests($requestObject->userDetailInterests);
+				$userDetail->setUserDetailRace($requestObject->userDetailRace);
+				$userDetail->setUserDetailReligion($requestObject->userDetailReligion);
+
+				// update the $reply->status $reply->message
 				}
 					catch
 					(\Exception | \TypeError $exception) {
@@ -148,8 +148,8 @@ use DeepDiveDatingApp\DeepDiveDating\UserDetail;
 					$reply->message = $exception->getMessage();
 												}
 
-					// encode and return reply to front end caller
-					header("Content-type: application/json");
+				// encode and return reply to front end caller
+				header("Content-type: application/json");
 					echo json_encode($reply);
 
 				// finally - JSON encodes the $reply object and sends it back to the front end.
