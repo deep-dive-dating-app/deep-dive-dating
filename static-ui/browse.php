fc2849286ea2ba2d45928a6b858fa09401f08624
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-13 10:13:40 -0600

    added browse to static ui

---
+++ static-ui/browse.php
@@ -0,0 +1,46 @@
+<?php require_once("head-utils.php"); ?>
+
+<?php //require_once("navbar.php"); ?>
+
+
+<main class="gray">
+	<div class="container">
+		<div class="container-fluid p-2">
+			<h1>Browse</h1>
+			<div class="row">
+				<div class="container">
+					<div class="card">
+						<img class="card-img-top" src="../src/img/unicorn.jpg" alt="Card image cap">
+						<div>
+							<h4 class="card-title">User - Name</h4>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="card">
+						<img class="card-img-top" src="../src/img/unicorn.jpg" alt="Card image cap">
+						<div class="card-block">
+							<h4 class="card-title">User - Name</h4>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class=" card">
+						<img class="card-img-top" src="../src/img/unicorn.jpg" alt="Card image cap">
+						<div class="card-block">
+							<h4 class="card-title">User - Name</h4>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="card">
+						<img class="card-img-top" src="../src/img/unicorn.jpg" alt="Card image cap">
+						<div class="card-block">
+							<h4 class="card-title">User - Name</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
