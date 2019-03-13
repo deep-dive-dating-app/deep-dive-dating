f65ae52bd22ea6b4c409891577807213013c3831
Taylor Smith <taylorleesmith92@gmail.com>
2019-03-13 10:09:45 -0600

    super basic navbar

---
+++ static-ui/navbar.php
@@ -0,0 +1,12 @@
+<header>
+	<nav class="navbar navbar-expand-lg navbar-dark">
+		<div class="navbar-brand" href="#">
+			<i>Logo</i>Deep Dive Dating</div>
+		<div class="text-center">
+			<a href="profile.php">Profile</a>
+			<a href="browse.php">Browse</a>
+			<a href="my-matches.php">My Matches</a>
+			<a href="sign-out.php">Sign Out</a>
+		</div>
+	</nav>
+</header>
