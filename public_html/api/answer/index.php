7ca02cd5a33a6a6af9b1026c1597441dd59b1c0f
Nehomah Mora <nehomahm@gmail.com>
2019-03-12 20:04:05 -0600

    finished answer api https://app.asana.com/0/1108530030234345/1108530030234349

--- public_html/api/answer/index.php
+++ public_html/api/answer/index.php
@@ -3,6 +3,7 @@ require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 use DeepDiveDatingApp\DeepDiveDating\{Answer, User};
@@ -33,65 +34,109 @@ try {
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize the input
-	$answerUserId = filter_input(INPUT_GET, "answerUserId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-	$answerQuestionId = filter_input(INPUT_GET, "answerQusetionId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$answerQuestionId = filter_input(INPUT_GET, "answerQuestionId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$answerResult = filter_input(INPUT_GET, "answerResult", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$answerScore = filter_input(INPUT_GET, "answerScore", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	//make sure the id is valid for methods that require it
-	if (($method === "DELETE" || $method === "PUT") && (empty($answerUserId) === true)){
+	if (($method === "DELETE" || $method === "PUT") && (empty($id) === true)){
 		throw(new InvalidArgumentException("Id cannot be empty or negative", 405));
 	}
+	// handle Get request - if id is present, that answer is returned, otherwise all beers are returned
+	if($method === "GET") {
+		// set XSRF cookie
+		setXsrfCookie();
 
-if($method === "POST") {
-	// enforce the user has a XSRF token
-	verifyXsrf();
+		// get a specific answer or all answers and update reply
+		if(empty($id) === false) {
+			$reply->data = Answer::getAnswerByAnswerUserId($pdo, $id);
+		} else if(empty($answerUserId, $answerQuestionId) === false) {
+			$reply->data = Answer::getAnswerByAnswerQuestionIdAndUserId($pdo, $answerUserId, $answerQuestionId);
+		}
+	} else if($method === "PUT"|| $method === "POST") {
 
-	//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-	$requestContent = file_get_contents("php://input");
+		// enforce the user has a XSRF token
+		verifyXsrf();
 
-	// This Line Then decodes the JSON package and stores that result in $requestObject
-	$requestObject = json_decode($requestContent);
+		//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+		$requestContent = file_get_contents("php://input");
 
-	//make sure answer result is available (required field)
-	if(empty($requestObject->answerResult) === true) {
-		throw(new \InvalidArgumentException ("There is no answer for this question.", 405));
+		// This Line Then decodes the JSON package and stores that result in $requestObject
+		$requestObject = json_decode($requestContent);
+
+		//make sure answer result is available (required field)
+		if(empty($requestObject->answerResult) === true) {
+			throw(new \InvalidArgumentException ("There is no answer for this question.", 405));
 	}
-	//make sure answer result is available (required field)
-	if(empty($requestObject->answerScore) === true) {
-		throw(new \InvalidArgumentException ("There is no score for this answer.", 405));
+		//make sure answer result is available (required field)
+		if(empty($requestObject->answerScore) === true) {
+			throw(new \InvalidArgumentException ("There is no score for this answer.", 405));
 	}
 
-	//perform the actual put or post
-	if($method === "PUT") {
+		//perform the actual put or post
+		if($method === "PUT") {
 
-		//retrieve the result to update
-		$answerResult = Answer::getAnswerByAnswerUserId($pdo, $answerUserId);
-		if($answer === null) {
-			throw (new RuntimeException("Answer does not exist", 404));
+			//retrieve the result to update
+			$answer = Answer::getAnswerByAnswerUserId($pdo, $id);
+			if($answer === null) {
+				throw (new RuntimeException("Answer does not exist", 404));
+			}
+
+			//enforce the user is signed in and only trying to edit their own answer
+			if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $answer->getAnswerUserId()->toString()) {
+				throw(new \InvalidArgumentException("You are not allowed to edit this answer.", 403));
+			}
+
+			//update all attributes
+			$answer->setAnswerResult($requestObject->answerResult);
+			$answer->setAnswerScore($requestObject->answerScore);
+			$answer->update($pdo);
+
+			//update reply
+			$reply->message = "Answer updated OK";
+	} else if($method === "POST") {
+
+			//enforce the user sign in
+			if(empty($_SESSION["user"]) === true) {
+				throw(new \InvalidArgumentException("You must be logged in to do that", 403));
+			}
+
+			validateJwtHeader();
+
+			//create new answer and insert it into the database
+			$answer = new Answer(generateUuidV4(), $_SESSION["user"]->getUserId(), $requestObject->answerResult, $requestObject->answerScore);
+
+			// update reply
+			$reply->message = "Answer created successfully";
+		}
+	}else if($method === "DELETE") {
+
+			// enforce the end user has an XSRF token
+			verifyXsrf();
+
+			// retrieve answer to be deleted
+			$answer = Answer::getAnswerByAnswerUserId($pdo, $id);
+			if($beer === null) {
+					throw(new RuntimeException("That answer does not exitst", 404));
+			}
+
+			// enforce the user is signed in and only trying to edith their own answer
+			if(empty($_SESSION["user"]) === true || $_SESSION["profile"]->getUserId()->toString() !== $answer->getAnswerUserId()->toString()) {
+				throw(new \InvalidArgumentException("you are not allowed to delete this answer.", 403));
+			}
+
+			//delete answer
+			$answer->delete($pdo);
+			//update reply
+			$reply->message = "Answer successfully deleted.";
+		} else {
+				throw(new \InvalidArgumentException("Invalid HTTP request"));
 		}
 
-		//enforce the user is signed in and only trying to edit their own answer
-		if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $answer->getAnserUserId()->toString()) {
-			throw(new \InvalidArgumentException("You are not allowed to edit this answer.", 403));
-		}
-
-		//update all attributes
-		$answer->setAnswerResult($requestObject->answerResult);
-		$answer->setAnswerScore($requestObject->answerScore);
-		$answer->update($pdo);
-
-		//update reply
-		$reply->message = "Answer updated OK";
-	}
-}	else {
-		throw(new \InvalidArgumentException("Invalid HTTP request", 418));
-
-	}
-	//update the $reply->status $reply->message
 }	catch(\TypeError | \Exception $exception) {
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
+			$reply->status = $exception->getCode();
+			$reply->message = $exception->getMessage();
 }
 
 //encode and return to the front end caller
