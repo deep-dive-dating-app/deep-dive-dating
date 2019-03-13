a9dfabf10939b178d1d36cde1361d060cdb4a604
Taylor Smith <taylorleesmith92@gmail.com>
2019-03-13 11:18:41 -0600

    working on report class

--- public_html/api/report/index.php
+++ public_html/api/report/index.php
@@ -41,11 +41,15 @@ try {
 	if($method === "GET") {
 		setXsrfCookie();
 
-		if (($reportAbuserId !== null) && ($reportUserId !== null)) {
-			$reply->data = Report::getReportByReportUserIdAndReportAbuserId($pdo, $reportUserId, $reportAbuserId)->toArray();
-		} else if($reportUserId !== null) {
+		if ($reportUserId !== null && $reportAbuserId !== null) {
+			$report = Report::getReportByReportUserIdAndReportAbuserId($pdo, $reportUserId, $reportAbuserId);
+			var_dump($report);
+			if($report !== null) {
+				$reply->data = $report;
+			}
+		} else if(empty($reportUserId) === false) {
 			$reply->data = Report::getReportByReportUserId($pdo, $reportUserId)->toArray();
-		} else if($reportAbuserId !== null) {
+		} else if(empty($reportAbuserId) === false) {
 			$reply->data = Report::getReportByReportAbuserId($pdo, $reportAbuserId)->toArray();
 		} else {
 			throw(new \InvalidArgumentException("Incorrect Search Parameters", 405));
@@ -61,8 +65,14 @@ try {
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
+		$reportAgent = $_SERVER['HTTP_USER_AGENT'];
 		if(empty($reportAgent) === true) {
 			throw(new \InvalidArgumentException("Report must have Agent info"));
+		}
+
+		$reportIp =  $_SERVER['SERVER_ADDR'];
+		if(empty($reportIp) === true) {
+			throw(new \InvalidArgumentException("Report must have an IP"));
 		}
 		if(empty($requestObject->reportContent) === true) {
 			throw(new \InvalidArgumentException("Report must have content"));
@@ -70,15 +80,10 @@ try {
 		if(empty($requestObject->reportDate) === true) {
 			$requestObject->reportDate = null;
 		}
-		if(empty($reportIp) === true) {
-			throw(new \InvalidArgumentException("Report must have an IP"));
-		}
-
 
 		// create new tweet and insert into the database
 		$reportId = generateUuidV4();
-		$reportAgent = $_SERVER['HTTP_USER_AGENT'];
-		$reportIp =  $_SERVER['SERVER_ADDR'];
+
 		$report = new Report($reportId, $_SESSION["user"]->getUserId(), $requestObject->reportAbuserId, $reportAgent, $requestObject->reportContent, $requestObject->reportDate, $reportIp);
 		$report->insert($pdo);
 		// update reply
