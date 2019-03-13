d82cf86be5f22824d2e6a32d79d988b604efd3db
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-13 10:45:33 -0600

    added my-matches and made some template cards

---
+++ static-ui/my-matches.php
@@ -0,0 +1,94 @@
+<?php require_once("head-utils.php"); ?>
+
+<?php //require_once("navbar.php"); ?>
+
+
+<main class="gray">
+	<div class="container">
+		<div class="container-fluid p-2">
+			<h1>Browse</h1>
+			<div class="row">
+				<div class="row border rounded">
+					<div class="col-sm-3">
+						<div class="container my-auto pt-5 d-block d-flex justify-content-center">
+							<img class="img-fluid" src="catgirl.jpg" alt="Catarina">
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="container my-auto py-5">
+							<p class="lorem"> bird bird bird human why take bird out i could have eaten that.</p>
+						</div>
+					</div>
+					<div class="col-sm-1">
+						<!--todo add letter grade-->
+						<p>letter grade</p>
+					</div>
+					<div class="col-sm-2">
+						<!--todo add matching ability to heart-->
+						<i class="far fa-heart"></i>
+					</div>
+				</div>
+				<div class="row border rounded">
+					<div class="col-sm-3">
+						<div class="container my-auto pt-5 d-block d-flex justify-content-center">
+							<img class="img-fluid" src="catgirl.jpg" alt="Catarina">
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="container my-auto py-5">
+							<p class="lorem"> bird bird bird human why take bird out i could have eaten that.</p>
+						</div>
+					</div>
+					<div class="col-sm-1">
+						<!--todo add letter grade-->
+						<p>letter grade</p>
+					</div>
+					<div class="col-sm-2">
+						<!--todo add matching ability to heart-->
+						<i class="far fa-heart"></i>
+					</div>
+				</div>
+				<div class="row border rounded">
+					<div class="col-sm-3">
+						<div class="container my-auto pt-5 d-block d-flex justify-content-center">
+							<img class="img-fluid" src="catgirl.jpg" alt="Catarina">
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="container my-auto py-5">
+							<p class="lorem"> bird bird bird human why take bird out i could have eaten that.</p>
+						</div>
+					</div>
+					<div class="col-sm-1">
+						<!--todo add letter grade-->
+						<p>letter grade</p>
+					</div>
+					<div class="col-sm-2">
+						<!--todo add matching ability to heart-->
+						<i class="far fa-heart"></i>
+					</div>
+				</div>
+				<div class="row border rounded">
+					<div class="col-sm-3">
+						<div class="container my-auto pt-5 d-block d-flex justify-content-center">
+							<img class="img-fluid" src="catgirl.jpg" alt="Catarina">
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="container my-auto py-5">
+							<p class="lorem"> bird bird bird human why take bird out i could have eaten that.</p>
+						</div>
+					</div>
+					<div class="col-sm-1">
+						<!--todo add letter grade-->
+						<p>letter grade</p>
+					</div>
+					<div class="col-sm-2">
+						<!--todo add matching ability to heart-->
+						<i class="far fa-heart"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
