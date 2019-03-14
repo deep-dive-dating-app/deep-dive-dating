8364dbafe79728b244dc2b37e145d15d04a9f13f
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-14 15:09:47 -0600

    created sign-up.ts

---
+++ ng/src/app/shared/interfaces/sign-up.ts
@@ -0,0 +1,22 @@
+export interface signUp{
+	userId: string,
+	userActivationToken: string,
+	userAgent: string,
+	userAvatarUrl: string,
+	userBlocked: number,
+	userEmail: string,
+	userHandle: string,
+	userHash: string,
+	userIpAddress: string
+	userDetailId: string;
+	userDetailUserId: string;
+	userDetailAboutMe: string;
+	userDetailAge: string;
+	userDetailCareer: string;
+	userDetailDisplayEmail: string;
+	userDetailEducation: string;
+	userDetailGender: string;
+	userDetailInterests: string;
+	userDetailRace: string;
+	userDetailReligion: string;
+}
