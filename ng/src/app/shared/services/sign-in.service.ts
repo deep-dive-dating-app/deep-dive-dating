85c1a9022f95fe00ee03df3521b70f1de86689d0
Taylor Smith <taylorleesmith92@gmail.com>
2019-03-14 16:10:23 -0600

    fixed directory issues

--- ng/src/app/shared/services/sign-in.service.ts
+++ ng/src/app/shared/services/sign-in.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs/Observable";
+import {Observable} from "rxjs";
 import {Status} from "../interfaces/status";
 import {SignIn} from "../interfaces/sign-in";
 
@@ -13,7 +13,11 @@ export class SignInService {
 	private signOutUrl = "api/sign-out/";
 
 	//preform the post to initiate sign in
-	postSignIn(signIn:SignIn) : Observable<Status> {
-		return(this.http.post<Status>(this.signInUrl));
+	postSignIn(signIn : SignIn) : Observable<Status> {
+		return(this.http.post<Status>(this.signInUrl, signIn));
+	}
+
+	getSignOut() : Observable<Status> {
+		return(this.http.get<Status>(this.signOutUrl));
 	}
 }
