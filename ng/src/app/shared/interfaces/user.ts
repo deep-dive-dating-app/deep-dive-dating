d8b500897faa0707e7d40b68d43cf311a82f8908
Taylor Smith <taylorleesmith92@gmail.com>
2019-03-14 14:33:34 -0600

    added user service and interface

---
+++ ng/src/app/shared/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface User{
+	userId: number,
+	userActivationToken: number,
+	userAgent: string,
+	userAvatarUrl: string,
+	userBlocked: number,
+	userEmail: string,
+	userHandle: string,
+	userHash: string,
+	userIpAddress: string
+}
---
+++ ng/src/app/shared/services/user.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {User} from "../interfaces/user";
+import {Status} from "../interfaces/status";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class UserService {
+	constructor(protected http: HttpClient) {
+	}
+
+	private userUrl = "https://bootcamp-coders.cnm.edu/~tsmith179/deep-dive-dating/public_html/api/user/";
+
+	getAllUsers() : Observable<User[]> {
+		return(this.http.get<User[]>(this.userUrl));
+	}
+
+	getUserByUserId(userId: string) : Observable<User> {
+		return(this.http.get<User>(this.userUrl + userId));
+	}
+
+	getUserByEmail(userEmail: string) : Observable<User> {
+		return(this.http.get<User>(this.userUrl + userEmail));
+	}
+
+	getUserByUserHandle(userHandle: string) : Observable<User> {
+		return(this.http.get<User>(this.userUrl + userHandle));
+	}
+
+	getUserbyUserActivationToken(userActivationToken : number) : Observable<User> {
+		return(this.http.get<User>(this.userUrl + userActivationToken));
+	}
+
+	createUser(user: User) : Observable<Status> {
+		return(this.http.post<Status>(this.userUrl, user));
+	}
+
+	deleteUser(userId: string) : Observable<Status> {
+		return(this.http.delete<Status>(this.userUrl + userId));
+	}
+
+	editUser(user: User) : Observable<Status> {
+		return(this.http.put<Status>(this.userUrl + user.userId, user))
+	}
+}
