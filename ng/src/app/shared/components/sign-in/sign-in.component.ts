75ef87557a282955b655414252044a7074b1ca5a
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-15 09:30:09 -0600

    created signIn component ts

---
+++ ng/src/app/shared/components/sign-in/sign-in.component.ts
@@ -0,0 +1,53 @@
+/*
+ this component is for signing up to use the site.
+ */
+
+//import needed modules for the sign-up component
+import {Component, OnInit, ViewChild,} from "@angular/core";
+import {Observable} from "rxjs/Observable"
+import {Router} from "@angular/router";
+import {Status} from "../../interfaces/status";
+import {SignIn} from "../../interfaces/sign-in";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SignInService} from "../../services/sign-in.service";
+
+
+// set the template url and the selector for the ng powered html tag
+@Component({
+	templateUrl: require("./sign-in.component.html")
+})
+
+export class SignInComponent implements OnInit{
+
+	signInForm : FormGroup;
+	status: Status = null;
+
+
+	constructor(private formBuilder : FormBuilder, private router: Router, private signInService: SignInService) {}
+
+	ngOnInit() : void {
+		this.signInForm = this.formBuilder.group({
+			userEmail: ["", [Validators.maxLength(128), Validators.required]],
+			userHash: ["", [Validators.maxLength(36), Validators.required]]
+		});
+
+	}
+
+	signIn(): void {
+
+		let signIn: SignIn = {userEmail: this.signInForm.value.userEmail, userHash: this.signInForm.value.userHash};
+
+		window.localStorage.removeItem("jwt-token");
+
+		this.signInService.postSignIn(signIn)
+			.subscribe(status => {
+				this.status = status;
+			if(status.status === 200) {
+				this.signInForm.reset();
+				console.log("Sign In Success!");
+			} else {
+				alert("Incorrect Email or Password. Please Try Again.")
+			}
+			});
+	}
+}
