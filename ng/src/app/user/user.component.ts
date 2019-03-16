10b9c6b260d618ab3f02ff27cddcc6deb24513b0
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-16 13:05:51 -0600

    worked on user component ts some more

--- ng/src/app/user/user.component.ts
+++ ng/src/app/user/user.component.ts
@@ -12,6 +12,8 @@ import {UserDetail} from "../shared/interfaces/user-detail";
 import {UserDetailService} from "../shared/services/user-detail.service";
 import {MatchService} from "../shared/services/match.service";
 import {AnswerService} from "../shared/services/answer.service";
+import {Answer} from "../shared/interfaces/answer";
+import {Match} from "../shared/interfaces/match";
 
 //set the template url and the selector for the ng powered html tag
 @Component({
@@ -22,26 +24,28 @@ import {AnswerService} from "../shared/services/answer.service";
 export class UserComponent implements OnInit{
 
 	jwtToken: any = this.jwt.decodeToken(localStorage.getItem("jwt-token"));
-	user: User = {userId: null, userHandle: null, userAvatarUrl: null};
-	userDetail: UserDetailService[];
-	//todo probably add match stuff, answer, question stuff, not sure yet
-	match: MatchService[];
-	answer: AnswerService = {answerScore: null};
+	user: User;
+	userDetail: UserDetail[];
+	match: Match[];
+	answer: Answer;
 	status: Status = {status:null, message:null, type:null};
 
 	constructor(private userService: UserService, private userDetailService: UserDetailService, private matchService: MatchService, private answerService: AnswerService, private jwt: JwtHelperService){}
 
 	ngOnInit() {
 		this.userService.getUserByUserId(this.jwtToken.auth.userId).subscribe(users => this.user = users);
+		this.loadAnswers();
 		this.loadUserDetail();
-		this.loadMatch();
-		this.answerService.getAnswer(this.jwtToken.auth.answerUserId).subscribe(Answers => this.answer = answers);
+		this.loadMatches();
+	}
+	loadAnswers() : void {
+		this.answerService.getAnswer(this.jwtToken.auth.answerUserId).subscribe(answers => this.answer = answers);
 	}
 	loadUserDetail() : void {
-		this.userDetailService.getUserDetailByUserId(this.jwtToken.auth.userId).subscribe(UserDetail => this.userDetail = userDetail);
+		this.userDetailService.getUserDetailByUserId(this.jwtToken.auth.userId).subscribe(userDetail => this.userDetail = userDetail);
 	}
 	loadMatches() : void {
-		this.matchService.getMatchByMatchUserId(this.jwtToken.auth.userId).subscribe(Match => this.match = matches);
+		this.matchService.getMatchByMatchUserId(this.jwtToken.auth.userId).subscribe(match => this.match = match);
 	}
 
 }
