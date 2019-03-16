1a5ef52ae3517cdbfd738a2c3ccb332781a71118
Nehomah Mora <nehomahm@gmail.com>
2019-03-16 11:04:37 -0600

    added jwt helper to app.module.ts https://app.asana.com/0/1108530030234345/1108530030234349

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -5,7 +5,20 @@ import { FormsModule } from '@angular/forms';
 import {ReactiveFormsModule} from "@angular/forms";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes"
 import { AppComponent } from './app.component';
+import {JwtModule} from "@auth0/angular-jwt";
 
+
+const JwtHelper = JwtModule.forRoot({
+  config : {
+    tokenGetter: () => {
+      return localStorage.getItem("jwt-token");
+    },
+    skipWhenExpired: true,
+    whitelistedDomains: ["localhost:7272", "https:bootcamp-coders.cnm.edu/"],
+    headerName: "X-JWT-TOKEN",
+    authScheme: ""
+  }
+});
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule, routing, ReactiveFormsModule],
