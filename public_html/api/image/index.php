bf0fc3524ebd5e0f223a4a2d5f727eece4f5f7fd
Nehomah Mora <nehomahm@gmail.com>
2019-03-18 10:58:28 -0600

    image upload changes https://app.asana.com/0/1108530030234345/1108530030234349

--- public_html/api/image/index.php
+++ public_html/api/image/index.php
@@ -1,13 +1,12 @@
 <?php
+
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
-use DeepDiveDatingApp\DeepDiveDating\{
-	User, Image
-};
+use DeepDiveDatingApp\DeepDiveDating\User;
 
 /**
  * Cloudinary Api for Images
@@ -22,54 +21,49 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 $reply = new StdClass();
 $reply->status = 200;
 $reply->data = null;
+
 try {
+	// determine the HTTP method used (we only allow the POST method to be used for image uploading
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort23/dateadan.ini");
-	$pdo = $secrets->getPdoObject();
-	$cloudinary = json_decode($config["cloudinary"]);
+	if ($method !== "POST") {
+		throw(new \Exception("This HTTP method is not supported for image upload.", 405));
+	}
+	
+	// verify the SXRF-TOKEN is present
+	verifyXsrf();
 
-	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
-
-	$userId = filter_input(INPUT_GET, "imageUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-
-	//$config = readConfig("/etc/apache2/capstone-mysql/cohort23/dateadan.ini");
-	\Cloudinary::config(["dt4vdvdap" => $cloudinary->cloudName, "586793469126844" => $cloudinary->apiKey, "pBInJOl4iRq6UQOeprKg8yW-yNw" => $cloudinary->apiSecret]);
-
-	if($method === "GET") {
-		setXsrfCookie();
-		$reply->data = Image::getAllImages($pdo)->toArray();
-		}
-
-		//get a specific image by id and update reply
-		/*if(empty($id) === false) {
-			$image = Image::getImageByImageId($pdo, $id);
-		} elseif(empty($tweetId) === false) {
-			$reply->data = Image::getImageByImageUserId($pdo, $userId)->toArray();
-		}*/
-		else if($method === "POST") {
-		//enforce that the end user has a XSRF token.
-		verifyXsrf();
-
-		//use $_Post super global to grab the needed Id
-		$userId = filter_input(INPUT_POST, "userId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-
-		// assigning variable to the user profile, add image extension
-		$tempUserFileName = $_FILES["image"]["tmp_name"];
-
-		// upload image to cloudinary and get public id
-		$cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 500, "crop" => "scale"));
-
-		// after sending the image to Cloudinary, create a new image
-		$image = new Image(generateUuidV4(), $userId, $cloudinaryResult["signature"], $cloudinaryResult["secure_url"]);
-		$image->insert($pdo);
-		// update reply
-		$reply->message = "Image uploaded Ok";
+	// make sure user is logged in before uploading a picture
+	if(empty($_SESSION["user"]) || empty($_SESSION["user"]->getUserId()->toString())) {
+		throw(new \InvalidArgumentException("You must be logged in to upload your image.", 403));
 	}
 
-} catch(Exception $exception) {
+	// validate header
+	validateJwtHeader();
+
+	$config = readConfig("/etc/apache2/capstone-mysql/cohort23/dateadan.ini");
+	$cloudinary = json_decode($config["cloudinary"]);
+	\Cloudinary::config(["dt4vdvdap" => $cloudinary->cloudName, "586793469126844" => $cloudinary->apiKey, "pBInJOl4iRq6UQOeprKg8yW-yNw" => $cloudinary->apiSecret]);
+
+	//assigning variable to the user image/avatar, add image extenstion
+	$tempUserFileName = $_FILES["image"]["tmp_name"];
+	// upload image to cloudinary and get public id
+	$cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 500, "crop" => "scale"));
+	$reply->data = $cloudinaryResult["secure_url"];
+
+	// update reply
+	$reply->message = "Image uploaded ok."
+
+} catch(Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
+
+//encode and return reply to the front-end caller
 header("Content-Type: application/json");
+if (!$reply->data) {
+	unset($reply->data);
+}
+
+// encode and return reply to front-end caller
 echo json_encode($reply);
