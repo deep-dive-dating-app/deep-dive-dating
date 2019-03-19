ad6959cce9c601bc181c2394f1f82d2aa843f4ec
Nehomah Mora <nehomahm@gmail.com>
2019-03-18 21:14:23 -0600

    about-us bootstrap/css changes https://app.asana.com/0/1108530030234345/1108530030234349

--- ng/src/app/my-matches/my-matches.component.ts
+++ ng/src/app/my-matches/my-matches.component.ts
@@ -1,24 +1,56 @@
 import {Component, OnInit} from "@angular/core";
 import {Status} from "../shared/interfaces/status";
-import {Match} from"../shared/interfaces/match"
+import {Match} from"../shared/interfaces/match";
 import {MatchService} from "../shared/services/match.service";
-import {User} from "../shared/interfaces/user"
+import {User} from "../shared/interfaces/user";
 import {Router} from "@angular/router";
+import {UserWithUserDetail} from "../shared/interfaces/userWithUserDetail";
+import {UserDetail} from "../shared/interfaces/user-detail";
+import {UserService} from "../shared/services/user.service";
 
 
 @Component({
 	templateUrl: ("my-matches.component.html"),
-	selector: "my-matches"
 })
 
 export class MyMatchesComponent implements OnInit{
-	user : User;
+	users: UserWithUserDetail[] = [];
+	user : User = {
+		userId: null,
+		userActivationToken: null,
+		userAgent: null,
+		userAvatarUrl: null,
+		userBlocked: null,
+		userEmail: null,
+		userHandle: null,
+		userHash: null,
+		userIpAddress: null
+	};
+	userDetail: UserDetail = {
+		userDetailId: null,
+		userDetailUserId: null,
+		userDetailAboutMe: null,
+		userDetailAge: null,
+		userDetailCareer: null,
+		userDetailDisplayEmail: null,
+		userDetailEducation: null,
+		userDetailGender: null,
+		userDetailInterests: null,
+		userDetailRace: null,
+		userDetailReligion: null
+	}
 	matches : Match[];
 	status : Status = {status: null, message: null, type: null};
 
-	constructor(private matchService : MatchService, private router : Router) {}
+	constructor(private userService: UserService, private matchService : MatchService, private router : Router) {}
 
 	ngOnInit() {
 		this.matchService.getMatchByMatchUserId(this.user.userId).subscribe(reply=> this.matches = reply);
+	}
+	getUsers() {
+		this.userService.getAllUsers().subscribe(reply => this.users= reply)
+	}
+	getDetailedView(user : UserWithUserDetail) : void {
+		this.router.navigate(["/user/", user.user.userId]);
 	}
 }
