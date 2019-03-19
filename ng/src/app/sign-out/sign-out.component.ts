a1577d0973ff0de7c06fb7f09d5e244aedf82017
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-03-19 14:25:31 -0600

    signout now signs you out

--- ng/src/app/sign-out/sign-out.component.ts
+++ ng/src/app/sign-out/sign-out.component.ts
@@ -1,17 +1,26 @@
 import {Component} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {SignInService} from "../shared/services/sign-in.service";
 
-@Component ({
+@Component({
 	templateUrl: "./sign-out.component.html",
 	selector: "sign-out"
 })
 
-export class SignOutComponent{
-	constructor (
-		protected route: ActivatedRoute
-	) {};
+export class SignOutComponent {
+	constructor(
+		protected router: Router, private signOutService: SignInService
+	) {
+	};
+
 	ngOnInit() {
 		//set session storage for sign up purposes
-		this.route.url.subscribe(route => window.sessionStorage.setItem("url", JSON.stringify(route)));
-	};
+		//this.route.url.subscribe(route => window.sessionStorage.setItem("url", JSON.stringify(route)));
+		this.signOutService.getSignOut().subscribe(status => {
+			if(status.status === 200) {
+				window.localStorage.removeItem("jwt-token")
+			this.router.navigate([""]);
+			}
+		})
+	}
 }
