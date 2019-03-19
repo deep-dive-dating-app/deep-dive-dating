d232830dc4a59283e0bd7dbe321cae2d21bdc004
Taylor Smith <taylorleesmith92@gmail.com>
2019-03-19 17:00:40 -0600

    routing fixes

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {SessionService} from "./shared/services/session.service";
 import {Status} from "./shared/interfaces/status";
+import {User} from "./shared/interfaces/user";
+import {UserService} from "./shared/services/user.service";
+import {AuthService} from "./shared/services/auth-service";
 
 @Component({
   selector: 'app-root',
   templateUrl: `./app.component.html`,
   styles: []
 })
+
 export class AppComponent {
-  status : Status = null;
-constructor(private sessionService: SessionService){
-  this.sessionService.setSession().subscribe(reply => this.status = reply)
-}
+	user: User;
+	userId: null;
+	status : Status = null;
+	constructor(private sessionService: SessionService, private userService: UserService, private authService: AuthService){
+		this.sessionService.setSession().subscribe(reply => this.status = reply);
+		this.userId = this.authService.decodeJwt().auth.userId;
+	}
 }
