9294788b8462b3f8ec0d6f28460a7ca30998ce71
Taylor Smith <taylorleesmith92@gmail.com>
2019-03-20 11:52:04 -0600

    worked on my profile link and removing fake errors

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -6,6 +6,7 @@ import {UserService} from "./shared/services/user.service";
 import {AuthService} from "./shared/services/auth-service";
 import {Router} from "@angular/router";
 import {Title} from "@angular/platform-browser";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Component({
   selector: 'app-root',
@@ -14,16 +15,21 @@ import {Title} from "@angular/platform-browser";
 })
 
 export class AppComponent {
-	userId : null;
+	user: User = null;
+	userId: string;
+	show: boolean = true;
 	router: Router;
 	status : Status = null;
-	constructor(private sessionService: SessionService, private userService: UserService, private authService: AuthService, private titleService: Title){
+	constructor(private sessionService: SessionService, private userService: UserService, private authService: AuthService, private titleService: Title, private jwtHelper: JwtHelperService) {
+		this.userId = this.jwtHelper.decodeToken(localStorage.getItem("jwt-token"));
 		this.sessionService.setSession().subscribe(reply => this.status = reply);
+		this.getUserId();
 		this.titleService.setTitle( "Dan's Den" );
 	}
 
 	getUserId() {
-		this.userId = this.authService.decodeJwt().auth.userId;
-		return
+		let token = localStorage.getItem("jwt-token");
+		token !== null ? this.userId = this.jwtHelper.decodeToken().auth.userId : this.userId = null
+
 	}
 }
